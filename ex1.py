93ec5ff801c97aca018ba78377106389d6063607
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-08 18:03:36 +0800

    update ex1.py file

--- ex1.py
+++ ex1.py
@@ -1,7 +1,12 @@
 # print("Hello World!")
-print("Hello Again")
+print("Hello Again") # is used for commenting
 print("I like typing this")
 print("This is fun")
 print('Yay! Printing')
 print("I'd much rather you 'not'")
 print('I "said" do not touch this')
+
+print("my pen", 3 + 5 / 2, 5 > 2, 6 <= 12 / 2) # returns: my pen 5.5 True True; note / and * was done first then + and -
+
+# % is the remainder division on integers
+print(4 % 2) # means divide 2 to groups of 4 that is impossible so returns 0
