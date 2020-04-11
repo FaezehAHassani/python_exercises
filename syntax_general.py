95cf24fe75dc864522a33426ea1ec21d4dcb9f62
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-11 12:19:18 +0800

    update syntax_general file

--- syntax_general.py
+++ syntax_general.py
@@ -1,4 +1,5 @@
-# print("Hello World!")
+# various types of print commands
+print("Hello World!")
 print("Hello Again") # is used for commenting
 print("I like typing this")
 print("This is fun")
@@ -6,12 +7,14 @@ print('Yay! Printing')
 print("I'd much rather you 'not'")
 print('I "said" do not touch this')
 
+# to add calculation to print command
 print("my pen", 3 + 5 / 2, 5 > 2, 6 <= 12 / 2) # returns: my pen 5.5 True True; note / and * was done first then + and -
 
 # % is the remainder division on integers
 print(4 % 2) # means divide 2 to groups of 4 that is impossible so returns 0
 
-cars = 100 # assign a avlue to a variable
+# assign a value to a variable
+cars = 100
 space_in_a_car = 4.0
 drivers = 30
 passengers = 90
@@ -20,6 +23,7 @@ cars_driven = drivers
 average_passengers_per_car = passengers / cars_driven
 print("we need to put about", average_passengers_per_car, "in each car")
 
+# format a string with defined variables
 my_name = 'Alex' # '' or "" can both be used for string
 my_degree = 'PhD'
 my_age = 30
@@ -39,5 +43,10 @@ print(f"if I said {x}")
 print(f"I also said '{y}'")
 hilarious = False # don't type FALSE
 print("isn't that joke funny?! {}".format(hilarious))
-#a = hilarious this is wrong even hilarious was poreviously defined, we cannot have variable on both side, one side should be integer ro sting
+#a = hilarious  this variable introduction is wrong even hilarious was poreviously defined, we cannot have variable on both side, one side should be integer ro sting
 print(x + y)
+
+print("I have", "." * 3, "eggs?") # returns I have ... eggs?
+end1 = "3"
+end2 = "eggs"
+print(end1 + ' ' + end2) # 3 eggs
