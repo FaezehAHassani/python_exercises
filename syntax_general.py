3b9c4ba958de06742b5a084cf75e13e661202234
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-13 11:48:55 +0800

    update general python syntax

--- syntax_general.py
+++ syntax_general.py
@@ -1,4 +1,4 @@
-# various types of print commands
+#### various types of print commands
 print("Hello World!")
 print("Hello Again") # is used for commenting
 print("I like typing this")
@@ -7,13 +7,13 @@ print('Yay! Printing')
 print("I'd much rather you 'not'")
 print('I "said" do not touch this')
 
-# to add calculation to print command
+#### to add calculation to print command
 print("my pen", 3 + 5 / 2, 5 > 2, 6 <= 12 / 2) # returns: my pen 5.5 True True; note / and * was done first then + and -
 
-# % is the remainder division on integers
+### % is the remainder division on integers
 print(4 % 2) # means divide 2 to groups of 4 that is impossible so returns 0
 
-# assign a value to a variable
+#### assign a value to a variable
 cars = 100
 space_in_a_car = 4.0
 drivers = 30
@@ -23,7 +23,7 @@ cars_driven = drivers
 average_passengers_per_car = passengers / cars_driven
 print("we need to put about", average_passengers_per_car, "in each car")
 
-# format a string with defined variables
+#### format a string with defined variables
 my_name = 'Alex' # '' or "" can both be used for string
 my_degree = 'PhD'
 my_age = 30
@@ -51,6 +51,7 @@ end1 = "3"
 end2 = "eggs"
 print(end1 + ' ' + end2) # 3 eggs
 
+#### formatting examples
 formatter = "{} {}"
 print(formatter.format(1, 2, 3, 4)) # format uses the formatter style and returns: 1 2
 
@@ -62,6 +63,7 @@ print(formatter.format(
     "new passion"
 ))
 
+#### introducing a list of strings
 days = "Mon Tue Wed Thu Fri Sat Sun"
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"  # \n will print the afterward string in the next line
 print("here is a list of the days in a week:   ", days) # the spaces will be printed exactly; returns: here is a list of the days in a week:    Mon Tue Wed Thu Fri Sat Sun
@@ -73,3 +75,12 @@ hi
 3
 today is Monday
 """)  # """ allows putting several strings in multiple lines, you cannot comment exactly after """ line, returns: each string in a seperate line
+
+#### escape double/single quote inside a string, various ways of using """
+print("I am 6'2\" tall") # returns: I am 6'2" tall
+
+var1 = "\tI'm tired" # \t introduces a tab
+var2 = """
+grocery list:\n\t$tomato\n\t$egg\n\t$onion
+"""
+print(var1, var2)
