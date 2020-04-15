99954d64cec9e33f3d7e21ef15685af0084ba2a1
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-15 14:50:46 +0800

    update syntax file for arument3-prompting and passing

--- syntax_argument3.py
+++ syntax_argument3.py
@@ -1,18 +1,18 @@
 # Prompting and passing
 
 from sys import argv
-script, user_name = argv
+script, user_name1, user_name2, user_name3 = argv
 prompt = '>' # you can enter whatever here that later will be used
 
-print(f"Hi {user_name}, I'm {script} script.")
+print(f"Hi {user_name1}, I'm {script} script.")
 print("I'd like to ask you a few questions.")
-print(f"Do you like me {user_name}?")
+print(f"Do you like me {user_name1}?")
 likes = input(prompt)
 
-print(f"Where do you live {user_name}?")
+print(f"Where do you live {user_name1}?")
 lives = input(prompt)
 
-print(f"What type of computer you have {user_name}?")
+print(f"What type of computer you have {user_name1}?")
 computer = input(prompt)
 
 print(f"""
@@ -20,3 +20,13 @@ Alright, you said {likes} about liking me.
 You live in {lives}. Not sure where is {lives} though?!
 And you have a {computer} computer. Nice.
 """)
+
+print(f"Hi {user_name2}. I was talking to {user_name1}")
+print(f"{user_name1} lives in {lives}.")
+print(f"Where do you live {user_name2}?")
+lives2 = input(prompt)
+
+print(f"Hi, {user_name3}. {user_name1} lives in {lives}, while {user_name2} lives in {lives2}. What about you, where do you live?")
+lives3 = input(prompt)
+
+print(f"Nice, you all live in {lives}!")
