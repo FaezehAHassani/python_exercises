182be9722c3206d53e41fd52dec3cb08256387a7
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-15 13:52:52 +0800

    update syntax file for argument

--- syntax_argument.py
+++ syntax_argument.py
@@ -1,9 +1,10 @@
 # Parameters, unpacking, variables
 
-from sys import argv   # calling an import to add features from python; agrv stands for argument variable and it hold the arguments you pass to Python script
-cript, first, second, third = argv # this command take whatever in agrv, unpack it, and assign it to the variables on the left in order
+from sys import argv   # calling an import to add sys module (library) from python; agrv stands for argument variable and it hold the arguments you pass to Python script
+script, first, second, third, fourth = argv # this command take whatever in agrv, unpack it, and assign it to the variables on the left in order
 
 print("the script called:", script)
 print("your first varaiable is:", first)
 print("your second variable is:", second)
-print("your thirs variable is:", third)
+print("your third variable is:", third)
+print("your fourth variable is", fourth)
