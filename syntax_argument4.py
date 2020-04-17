b6856aba56d5d32119d4044ed6aa57d25ef9c223
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-17 18:00:19 +0800

    update syntax4 file for argument

--- syntax_argument4.py
+++ syntax_argument4.py
@@ -6,7 +6,9 @@ script, filename = argv
 txt = open(filename) # open the file
 
 print(f"Here's your file {filename}:")
-print(txt.read())  # print the file content
+print(txt.read())  # read and print the file content
+
+print(txt.close()) # it closed the file and returns None
 
 print("Type the filename again:")
 file_again = input(">")
@@ -14,6 +16,7 @@ file_again = input(">")
 txt_again = open(file_again)
 
 print(txt_again.read())
+print(txt.close())
 
 print("Type the filename again:")
 file_again2 = input() # without prompt
@@ -21,9 +24,4 @@ file_again2 = input() # without prompt
 txt_again2 = open(file_again2)
 
 print(txt_again2.read())
-
-close(txt)
-
-close(txt_again)
-
-close(txt_again2)
+print(txt.close())
