d7bbdbddf1adfe723fd4eede2f0be1d9a94312c0
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-17 17:04:59 +0800

    update syntax4 file for argument- file reading

--- syntax_argument4.py
+++ syntax_argument4.py
@@ -3,10 +3,14 @@
 from sys import argv
 script, filename = argv
 
-txt = open(filename)
+txt = open(filename) # open the file
 
 print(f"Here's your file {filename}:")
-print(txt.read())  # read the files
+print(txt.read())  # print the file content
 
 print("Type the filename again:")
 file_again = input(">")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
