02fa83e8f0db3c5dcaaa34c1454f64c1ee79636e
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-17 17:21:38 +0800

    update syntax4 file for argument

--- syntax_argument4.py
+++ syntax_argument4.py
@@ -14,3 +14,16 @@ file_again = input(">")
 txt_again = open(file_again)
 
 print(txt_again.read())
+
+print("Type the filename again:")
+file_again2 = input() # without prompt
+
+txt_again2 = open(file_again2)
+
+print(txt_again2.read())
+
+close(txt)
+
+close(txt_again)
+
+close(txt_again2)
