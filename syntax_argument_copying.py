6442e5c6aed9070ecf6f06c49c5340b6f3941c90
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-20 09:49:06 +0800

    create a syntax file for copying files usimg argument

--- syntax_argument_copying.py
+++ syntax_argument_copying.py
@@ -8,5 +8,15 @@ script, from_fle, to_file = argv
 print(f"copying from {from_file} to {to_file)}.")
 
 # the below two lines can be replaced by:  indata = open(from_file).read()
-in_file = open(from_file)
-indata = in_file.read()
+in_file = open(from_file) # open from_file
+indata = in_file.read()   # read from_fle
+
+print(f"The input file is {len(indata)} bytes long") # len(indata) gives the bytes lenght of data
+
+print(f"Does the output file exist?{exists(to_file)}")
+
+print("ready, hit RETURN to continue, CTRL-C to abort")
+input()
+
+out_file = open(to_file, "w")
+out_file.write(indata)  # write whatever read from from_file to out_file
