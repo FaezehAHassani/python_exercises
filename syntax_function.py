9d17c925d57f3d301b1dd3c167e983c17e069cad
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-22 11:48:10 +0800

    create a syntax file on amking a function

---
+++ syntax_function.py
@@ -0,0 +1,6 @@
+# Functions
+
+# define a function named "print_two"
+def print_two(*args): # this will ask for two arguments from your
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2})
