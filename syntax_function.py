41d132b1fdfff885be617b42c8653b9972fe387f
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-22 11:53:55 +0800

    update syntax file on amking a function

--- syntax_function.py
+++ syntax_function.py
@@ -1,6 +1,10 @@
 # Functions
 
-# define a function named "print_two"
+# define a function named "print_two" that gets two arguments: Zed and Shaw and returnts arg1: Zed, arg2: Shaw
 def print_two(*args): # this will ask for two arguments from your
     arg1, arg2 = args
-    print(f"arg1: {arg1}, arg2: {arg2})
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+
+
+print_two("Zed", "Shaw")
