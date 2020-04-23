c5e13f60d24c710a1322fd7212876e983709a646
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-23 11:53:52 +0800

    update syntax file2 for function

--- syntax_function2.py
+++ syntax_function2.py
@@ -18,18 +18,14 @@ cheese_and_crackers(10 + 20, 10 / 2)
 print("We can even combine variables and numbers for a function values")
 cheese_and_crackers(amount_of_cheese + (100 / 3), amount_of_crackers - (30 * 2))
 
-def full_name(first_name, last_name):
-    print(f"This is your first name: {first_name}.\n")
-    print(f"This is your last name: {last_name}.\n")
-    print(f"Your complete name is: {first_name} {last_name} ")
+# make a function to make full name with title
+def title_full_name(title, first_name, last_name):
+    print(f"This is your title: {title}\n")
+    print(f"This is your first name: {first_name}\n")
+    print(f"This is your last name: {last_name}\n")
+    print(f"Your full name with title is: {title} {first_name} {last_name}")
 
-surname = "liam"
-family = "nelson"
-complete_name = full_name(surname, family)
-
-def title_full_name(title, complete_name):
-    complete_name = full_name(surname, family)
-    print(f"Your fulle name with title is: {title} {complete_name}")
-
-your_title = input("Your title is:")
-title_full_name(your_title, complete_name)
+surname = "Liam"
+family = "Nelson"
+title = "Dr"
+title_full_name(title, surname, family)
