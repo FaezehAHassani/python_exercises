e82e7d1cbcd99c6ecba37532166a2ae99da92b4d
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-23 10:12:12 +0800

    update syntax file2 for function

--- syntax_function2.py
+++ syntax_function2.py
@@ -1,22 +1,13 @@
-# Functions
+# Functions and variables
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    print(f"You have {cheese_count} cheeses and {boxes_of_crackers} boxes of crackers!")
+    print("Girl that's enough for a party!")
+    print("Get a blanket for the picnic party\n")
 
-# define a function named "print_two" that gets two arguments: "Zed", "Shaw" and returnts arg1: Zed, arg2: Shaw
-def print_two(*args):
-    arg1, arg2 = args
-    print(f"arg1: {arg1}, arg2: {arg2}")
-print_two("Zed", "Shaw")
+print("Let's give numbers to the function")
+cheese_and_crackers(20, 40)
 
-# define a function named "print_two_again" that gets two arguments: "Zed", "Shaw" and returnts arg1: Zed, arg2: Shaw
-def print_two_again(arg1, arg2):
-    print(f"arg1: {arg1}, arg2: {arg2}")
-print_two_again("Zed", "Shaw")
-
-# define a function named "print_one" that gets one argument: "First!" and returnts arg1: First!
-def print_one(arg1):
-    print(f"arg1: {arg1}")
-print_one("First!")
-
-# define a function named "print_none" that gets nothing and returnts arg1: I got nothin'.
-def print_none():
-    print("I git nothin'.")
-print_none()
+print("Let's give variables to the function")
+amount_of_cheese = 10
+amount_of_crackers = 30
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
