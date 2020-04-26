ac1ddbf160f11a10d4acd4318a26ca2ed2d29749
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-26 14:19:04 +0800

    update the syntax3 file for function- reading and printing a text file

--- syntax_function3.py
+++ syntax_function3.py
@@ -1,14 +1,14 @@
 # Functions and files
 
-from sys import agrv
+from sys import argv
 
-script, input_file = agrv
+script, input_file = argv
 
 def print_all(f):
     print(f.read())
 
 def rewind(f):  # rewind is like rewinding a tape/ go back to the tape beginning
-    f.seek(0)
+    f.seek(0) # remmber seek(0) takes us to the first line of the sript
 
 def print_a_line(line_count,f):
     print(line_count, f.readline())
@@ -20,3 +20,14 @@ print_all(current_file)
 
 print("Let's rewind the file")
 rewind(current_file)
+
+print("Let's print three lines, line by line:")
+
+current_line = 1  # later this may be replaced with a for loop!
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
