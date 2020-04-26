20fa1ca83302ef0e6d796f12d3e954886bc5187d
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-26 14:10:25 +0800

    update the syntax3 file for function- reading and printing a text file

--- syntax_function3.py
+++ syntax_function3.py
@@ -1,31 +1,22 @@
-# Functions and variables
-def cheese_and_crackers(cheese_count, boxes_of_crackers):
-    print(f"You have {cheese_count} cheeses and {boxes_of_crackers} boxes of crackers!")
-    print("Girl that's enough for a party!")
-    print("Get a blanket for the picnic party\n")
+# Functions and files
 
-print("Let's give numbers to the function")
-cheese_and_crackers(20, 40)
+from sys import agrv
 
-print("Let's give variables to the function")
-amount_of_cheese = 10
-amount_of_crackers = 100
-cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+script, input_file = agrv
 
-print("We can even do maths for a function values")
-cheese_and_crackers(10 + 20, 10 / 2)
+def print_all(f):
+    print(f.read())
 
-print("We can even combine variables and numbers for a function values")
-cheese_and_crackers(amount_of_cheese + (100 / 3), amount_of_crackers - (30 * 2))
+def rewind(f):  # rewind is like rewinding a tape/ go back to the tape beginning
+    f.seek(0)
 
-# make a function to make full name with title
-def title_full_name(title, first_name, last_name):
-    print(f"This is your title: {title}\n")
-    print(f"This is your first name: {first_name}\n")
-    print(f"This is your last name: {last_name}\n")
-    print(f"Your full name with title is: {title} {first_name} {last_name}")
+def print_a_line(line_count,f):
+    print(line_count, f.readline())
 
-surname = "Liam"
-family = "Nelson"
-title = "Dr"
-title_full_name(title, surname, family)
+current_file = open(input_file)
+
+print("Let's first print the whole file:\n")
+print_all(current_file)
+
+print("Let's rewind the file")
+rewind(current_file)
