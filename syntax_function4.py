3480c2641c9265d851832190083d3134bad1e162
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-28 16:48:29 +0800

    create a syntax file for functions- returning something

---
+++ syntax_function4.py
@@ -0,0 +1,11 @@
+# Functions for returning something
+
+def add(a, b):
+    print(f"adding {a} + {b}")
+    return a + b
+
+def subtract(a, b):
+    print(f"subtracting {a} - {b}")
+    return a - b
+
+    
