7fc57f814b7000798cfc8d76e6bc3ddaa6dee4ad
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-30 14:30:47 +0800

    update syntax file for encoding

---
+++ syntax_encoding.py
@@ -0,0 +1,20 @@
+# encode a script by using UTF-8 encoding with "strict" error
+
+import sys
+script, input_encoding, error = sys.argv
+
+def main(language_file, encoding, errors):
+    line = language_file.readline()
+    if line:
+       print_line(line, encoding, errors)
+       return main(language_file, encoding, errors)
+
+def print_line(line, encoding, errors):
+    next_lang = line_strip()
+    raw_bytes = next_leng.encode(encoding, errors = errors)
+    cooked_string = raw_bytes.decode(encoding, errors = errors)
+    print(raw_bytes, "<==>", cooked_string)
+
+languages = open("languages.txt", encoding = "utf-8")
+
+main(languages, input_encoding, error)
