c643fe335df18fd559cb0cd97033f14cd22c5dbb
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-02 23:03:11 +0800

    update syntax file for practicing python codes

--- syntax_practice.py
+++ syntax_practice.py
@@ -5,7 +5,14 @@ print('\n newlines and  \t tabs')
 poem = """
 \tThe lovely world
 with logic so firmly planted
-cannot discern \n the needs of lovelynor comprehend passion from intuition
+cannot discern \n the needs of love
+nor comprehend passion from intuition
 and requires an explanation
 \n\t\t where there is none.
 """
+print("---------")
+print(poem)
+print("---------")
+
+five = 10 - 2 + 3 - 6
+print(f"This should be five: {five}")
