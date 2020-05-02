997126de105ebdfd67d120977a475f6c0d905e56
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-02 22:59:00 +0800

    create a syntax file to practice python codes so far

---
+++ syntax_practice.py
@@ -0,0 +1,11 @@
+print("Let's practice everything.")
+print('You\'d need to know \'bout escapses with \\ that do:')
+print('\n newlines and  \t tabs')
+
+poem = """
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of lovelynor comprehend passion from intuition
+and requires an explanation
+\n\t\t where there is none.
+"""
