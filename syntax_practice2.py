243dc96f07de3df543805bf852de3357f0649210
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-04 16:01:22 +0800

    update the syntax file for more practice on functions

--- syntax_practice2.py
+++ syntax_practice2.py
@@ -11,3 +11,13 @@ def print_first_word(words):
     """Prints the first word after popping it off."""
     word = words.pop(0)
     print(word)
+
+def print_last_word(words):
+    """Prints last worf after popping it off."""
+    word = word.pop(-1)
+    print(word)
+
+def sort_sentence(sentence):
+    """Takes in a full sentenceand returns the sorted words."""
+    words = break_words(sentence)
+    return sort_words(sentence)
