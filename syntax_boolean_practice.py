25aa6ec324ad2d2fa030b8e0814cefa1174ee7be
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-07 17:14:53 +0800

    update the syntax file for  practicing boolean logic

--- syntax_boolean_practice.py
+++ syntax_boolean_practice.py
@@ -1,13 +1,16 @@
-True and True # True
-False and True # False
-1 == 1 and 2 == 1 # False
-"test" == "test" # True
-1 == 1 or 2 != 1 # True
-True and 1 == 1 # True
-False and 0 != 0 # False
-True or 1 == 1 # True
-"test" == "testin" # False
-1 != 0 and 2 == 1 # False
-"test" != "testing" # True
-"test" == 1 # False
-not (True and False) # True
+print(True and True) # True
+print(False and True) # False
+print(1 == 1 and 2 == 1) # False
+print("test" == "test") # True
+print(1 == 1 or 2 != 1) # True
+print(True and 1 == 1) # True
+print(False and 0 != 0) # False
+print(True or 1 == 1) # True
+print("test" == "testing") # False
+print(1 != 0 and 2 == 1) # False
+print("test" != "testing") # True
+print("test" == 1) # False
+print(not (True and False)) # True
+print(not ( 1 == 1 and 0 != 1)) # False
+print(not (10 == 1 or 1000 == 1000)) # False
+print(not (1 != 10 or 3 == 4)) # False
