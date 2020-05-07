ceba6536846ba7f69c972518afc1521c6faec7d0
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-07 17:08:50 +0800

    update the syntax file for  practicing boolean logic

--- syntax_boolean_practice.py
+++ syntax_boolean_practice.py
@@ -4,3 +4,10 @@ False and True # False
 "test" == "test" # True
 1 == 1 or 2 != 1 # True
 True and 1 == 1 # True
+False and 0 != 0 # False
+True or 1 == 1 # True
+"test" == "testin" # False
+1 != 0 and 2 == 1 # False
+"test" != "testing" # True
+"test" == 1 # False
+not (True and False) # True
