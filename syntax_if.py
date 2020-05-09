cacc02c596daa7e88950ca702de2acb7f20da3d9
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-09 22:20:20 +0800

    create syntax file for if-statement

--- syntax_if.py
+++ syntax_if.py
@@ -1,3 +1,11 @@
-# if clause
+# if-statement
 
-people
+people = 35
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("Too many cats! The world is doomed!")
+
+if people > cats:
+    print("Not many cats! The world is saved!")
