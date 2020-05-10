28315847191c0501aac6c983b69a2dcb052a29d1
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-10 16:19:55 +0800

    update syntax file2 for if else-statement to make decisions

--- syntax_if_else2.py
+++ syntax_if_else2.py
@@ -1,14 +1,21 @@
 # if else-statement
 
-print(""" you enter a dark room with two doors.
+print(""" You enter a dark room with two doors.
 Do you go through door #1 or door #2?""")
 
-door = inpoot(">")
+door = input(">")
 
-if door == 1:
+if door == "1":
     print("There is a giant bear here eating a cheesecake.")
     print("What do you do?")
     print("1. Take the cake.")
     print("2. Scream at the bear.")
 
-input(">")
+    bear = input(">")
+    if bear == "1":
+        print("The bear eats your face off. Good job!")
+    elif bear == "2"
+    print("The bear eats your legs off. Good job!")
+    else:
+        print(f"Well, doing {bear} is better.")
+        print("Bear runs away.")
