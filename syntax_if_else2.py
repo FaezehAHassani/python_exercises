626a70ed048a48e3336a0a7bf91ac2ce0e294d44
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-10 16:37:26 +0800

    update syntax file2 for if else-statement to make decisions

--- syntax_if_else2.py
+++ syntax_if_else2.py
@@ -24,7 +24,7 @@ elif door == "2":
     print("You stare into the endless abyss at Cthulhu's retina.")
     print("1. Blueberries.")
     print("2. Yello jacket clothespins.")
-    print("3. understanding revolvers yelling melodies.")
+    print("3. Understanding revolvers yelling melodies.")
 
     insanity = input(">")
 
@@ -36,3 +36,21 @@ elif door == "2":
 
 else:
     print("You must stumble around and fall on a knife and die. Good job!")
+
+# My new game
+
+print("""You are hungry, you ask your mom for a snack.
+Your mom says, do one of below tasks or no snack!""")
+
+print("1. Tidy yout room.")
+print("2. Wash the dishes.")
+print("3. Study.")
+
+horror = input(">")
+
+if horror == "1":
+    print("Damn, again mom!!! I just cleaned it last week.")
+elif horror == "2":
+    print("Cannot, my hand aches, please give me a snackkkkkk.")
+else:
+    print("I go to study for 10 min then come back for my snack prize, is it fair?")
