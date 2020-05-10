05fe38d1428ab9159a40b81ab20e541f78e66b78
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-10 16:02:22 +0800

    update syntax file for if else-statement

--- syntax_if_else.py
+++ syntax_if_else.py
@@ -1,1 +1,12 @@
 # if else-statement
+
+people = 30
+cars = 40
+trucks = 15
+
+if cars > people:
+    print("We should take cars.")
+elif cars < people:
+    print("We should not take cars.")
+else:
+    print("We can't decide") 
