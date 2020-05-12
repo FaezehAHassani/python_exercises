bac2079621f8a22c89dc444374aa9dffbdc3e15a
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-12 15:36:09 +0800

    create syntax file for loop

---
+++ syntax_loop.py
@@ -0,0 +1,24 @@
+# if else-statement
+
+people = 30
+cars = 40
+trucks = 15
+
+if cars > people:
+    print("We should take cars.")
+elif cars < people:
+    print("We should not take cars.")
+else:
+    print("We can't decide")
+
+if trucks > cars:
+    print("That's too many trucks.")
+elif trucks < cars:
+    print("Maybe we could take the trucks.")
+else:
+    print("We still can't decide. ")
+
+if people > trucks:
+    print("Alright, let's just take the trucks.")
+else:
+    print("Fine, let's stay home then.")
