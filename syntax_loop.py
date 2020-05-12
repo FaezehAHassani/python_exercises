55c4b7b6559026adf597e58cb7666cc0ad6ee8c2
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-12 15:57:38 +0800

    update syntax file for loop

--- syntax_loop.py
+++ syntax_loop.py
@@ -1,6 +1,6 @@
 # list and loop
 
-the_count = [ 1, 2, 3, 4, 5]
+the_count = [ 1, 2, 3, 4, 5]  # making a list
 fruits = ['apples', 'oranges', 'pears', 'apricots']
 change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
 
@@ -11,3 +11,12 @@ for number in the_count:   # basically putting "the_count" contents in variable 
 
 for fruit in fruits:
     print(f"A fruit of type {fruit}")
+
+for i in change:
+    print(f"I got {i}")
+
+elements = []  # make an empty list
+
+for i in range(0, 6):  # put the numbers in the range of 0 t0 6 inside variable "i"
+    print(f"Adding {i} to the list.")
+    elements.append(i) # append will add contents in "i" to the empty list "elements"
