6226050ba65e572c4fdcc5e0be1ba63c8f9b8f79
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-18 11:09:12 +0800

    create FizzBuzz test for a list of 100 numbers

---
+++ syntax_while_loop_buzzfizz.py
@@ -0,0 +1,30 @@
+# print a list of numbers from 1-100, if the number divisible by 15 print FizzBuzz, it divisible by 3 print Fizz, and if divisible by 5 print Buzz
+
+i = 1
+numbers = []
+
+while i < 101:
+    numbers.append(i)
+    i = i + 1
+
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
+
+print("Let's go for the FizzBuzz test!")
+
+j = 0
+
+while j < 100:
+    if numbers[j] % 15 == 0:
+        print("FizzBuzz")
+
+    if numbers[j] % 5 == 0:
+        print("Buzz")
+    elif numbers[j] % 3 == 0:
+        print("Fizz")
+    else:
+        print(numbers[j])
+
+    j = j + 1
