62e480d387e28e684fbcaa75c53cfd08e8a83604
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-18 11:09:52 +0800

    create a test filr for FizzBuzz.py file

---
+++ syntax_while_loop_buzzfizz_test.py
@@ -0,0 +1,18 @@
+# test for syntax_while_loop_buzzfizz.py
+
+numbers = [2, 15, 9, 6, 5]
+
+j = 0
+
+while j < 5:
+    if numbers[j] % 15 == 0:
+        print("FizzBuzz")
+
+    if numbers[j] % 5 == 0:
+        print("Buzz")
+    elif numbers[j] % 3 == 0:
+        print("Fizz")
+    else:
+        print(numbers[j])
+
+    j = j + 1
