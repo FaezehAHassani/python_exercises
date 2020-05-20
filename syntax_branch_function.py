8128efe8abdbafcbaae16212b48ba110af43d086
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-20 11:03:05 +0800

    update syntax file for branches and functions

--- syntax_branch_function.py
+++ syntax_branch_function.py
@@ -52,3 +52,19 @@ def bear_room():
 def dead(why):
     print(why, "Good job!")
     exit(0)  # this will exit the .py script
+
+def start():
+    print("You are in a dark room.")
+    print("There is a door to your right and left.")
+    print("Which one do you take?")
+
+    choice = input("> ")
+
+    if choice == "left":
+        bear_room()
+    elif choice == "right":
+        cthulhu_room()
+    else:
+        dead("You stumble around the room until you starve.")
+
+start()
