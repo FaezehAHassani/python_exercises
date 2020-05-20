fba5d1ce513b1cf5539f4d93c340ed5e63c485a3
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-20 10:35:03 +0800

    create syntax file for branches and functions

--- syntax_branch_function.py
+++ syntax_branch_function.py
@@ -1,1 +1,13 @@
 # branches and functions
+
+from sys import exit
+
+# Importent note: first define a few functions but they are not called untill reaching the final line of start()
+def gold_room():
+    print("This room is full of gold. How much do you take?")
+
+    choice = input("<")
+    if "0" in choice or "1" in choice:
+        how_much = int(choice)
+    else:
+        dead("Man, learn to type a number.")
