baba082cfce8190e222e37532d201afe6f616206
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-22 11:47:41 +0800

    update syntax file for symbol review

--- syntax_symbol_review.py
+++ syntax_symbol_review.py
@@ -1,1 +1,17 @@
 # symbol review
+
+True and False # returns False
+
+# part of with-as statement
+book_numbers = 100
+with book_numbers as my_list_fav:
+    my_list_fav / 10
+
+# ensure sth is True
+assert False, "Error!"
+
+# stop the loop
+while True: break
+
+# define a class that is "Person" and assign an object that is "object" to it
+class Person(object)
