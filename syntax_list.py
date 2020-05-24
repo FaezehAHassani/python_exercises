e9daed6a5963ce8c6633026769ed5cf16b1fefa7
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-24 10:39:30 +0800

    update syntax file for list- doing things to list

--- syntax_list.py
+++ syntax_list.py
@@ -8,9 +8,10 @@ stuff = ten_things.split(' ') # if I put '' it give an error
 print(stuff)
 
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
+print(more_stuff)
 
 while len(stuff) != 10:
-    next_one = more_stuff.pop() # delete the last index from more_stuff
+    next_one = more_stuff.pop() # remove the last index from more_stuff and store it in next_one
     print("Adding: ", next_one)
     stuff.append(next_one) # adds next_one list to the end of stuff list
     print(f"There are {len(stuff)} items now.")
