f08606801fcd0426c8b2422f3c38bdcf5edde1e7
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-24 10:29:01 +0800

    update syntax file for list- doing things to list

--- syntax_list.py
+++ syntax_list.py
@@ -4,5 +4,7 @@ ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
 print("Wait there are not 10 things in that list. Let's fix that.")
 
-stuff = ten_things.split('')
+stuff = ten_things.split(' ') # if I put '' it give an error
+print(stuff)
+
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
