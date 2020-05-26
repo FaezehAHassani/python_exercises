625cc92caa29a8d10b711d24b7eead5e52c71000
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-26 09:39:45 +0800

    update syntax file for dictionary

--- syntax_dictionary.py
+++ syntax_dictionary.py
@@ -1,1 +1,9 @@
 # dictionary: a lisst that not only contains numbers but strings
+
+things = ['a', 'b', 'c', 'd']
+print(things[1]) # returns b
+
+things[1] = 'z' # replace things[1] with z
+print(things[1]) # returns z
+
+things # returns ['a', 'z', 'c', 'd']
