618907af4038fc89c0ee3163d046f4b4f95e3894
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-26 09:45:55 +0800

    update syntax file for dictionary

--- syntax_dictionary.py
+++ syntax_dictionary.py
@@ -1,5 +1,6 @@
 # dictionary: a lisst that not only contains numbers but strings
 
+# example 1
 things = ['a', 'b', 'c', 'd']
 print(things[1]) # returns b
 
@@ -7,3 +8,14 @@ things[1] = 'z' # replace things[1] with z
 print(things[1]) # returns z
 
 things # returns ['a', 'z', 'c', 'd']
+
+# example 2
+stuff = {'name': 'Zed', 'age': 39, 'height': 6 * 12 + 2} # remember it uses {} not []
+print(stuff['name'])
+
+print(stuff['age'])
+
+print(stuff['height'])
+
+stuff['city'] = 'SF'
+print(stuff['city'])
