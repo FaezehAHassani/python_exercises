38a0aa0f563af26a02b3041527199d69aef1ae48
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-28 15:53:35 +0800

    update syntax file for classes and modules

--- syntax_class.py
+++ syntax_class.py
@@ -1,6 +1,13 @@
-# classes and modules
+ # classes and modules
 
 import mystuff
 mystuff.apple()
 print(mystuff.tangerine)
- 
+
+class Mystuff(object):
+
+    def _init_(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apple(self):
+        print("I AM CLASSY APPLES!")
