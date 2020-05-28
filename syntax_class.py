d18f2094dd317d26f7495c41807b5f66c20e8c3c
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-28 16:19:23 +0800

    update syntax file for classes and modules

--- syntax_class.py
+++ syntax_class.py
@@ -29,7 +29,7 @@ class Song(object):
 
 happy_birthday = Song(["Happy birthday to you",
                         "I don't want to get sued",
-                        "So I'll stop rigth there!"])
+                        "So I'll stop right there!"])
 
 bulls_on_parade = Song(["They rally around the family",
                          "With pockets full of shells!"])
