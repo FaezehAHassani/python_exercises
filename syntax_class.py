6eb8dc1cd446cbb76577e02a4f81bc93e0df776f
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-28 16:17:58 +0800

    update syntax file for classes and modules

--- syntax_class.py
+++ syntax_class.py
@@ -16,3 +16,26 @@ class Mystuff(object):
 thing = Mystuff()
 thing.apple()
 print(thing.tangerine)
+
+# another example of class
+class Song(object):
+
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+happy_birthday = Song(["Happy birthday to you",
+                        "I don't eant to get sued",
+                        "so I'll stop rigth there!"])
+
+bulls_on_parade = Song(["They rally around the family",
+                         "with pockets full of shells"])
+
+happy_birthday.sing_me_a_song()
+
+print(happy_birthday.lyrics) # returns ['Happy birthday to you', "I don't eant to get sued", "so I'll stop rigth there!"]
+
+bulls_on_parade.sing_me_a_song()
