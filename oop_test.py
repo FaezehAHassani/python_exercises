b190ee44bdaa634520cd898098b096ab1902e366
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-30 15:39:08 +0800

    updateoop_test.py file as a reading text

--- oop_test.py
+++ oop_test.py
@@ -1,1 +1,23 @@
 # a reading test
+
+import random
+from urllib.request import urlopen
+import sys
+
+WORD_URL = "http://learncodethehardway.org/words.text"
+WORDS = []
+
+PHRASES = {
+"class %%%(%%%): ":
+ "Make a class named %%% that is-a %%%.",
+"class %%%(object):\n\tdef__init__(self, ***)":
+ "class %%% has-a __init__ that takes self and *** params.",
+"class %%%(object):\n\tdef ***(self, @@@)":
+ "class %%% has-a function *** that takes self and @@@ params.",
+"*** = %%%()":
+ "set *** to an instance of class %%%",
+"***.***()":
+ "from *** get the *** function, call it with params self, @@@",
+"***.*** = '***'":
+ "from *** get the *** attricute and set it to '***'"
+}
