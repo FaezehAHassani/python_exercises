462996e434bb8849632dda99c76b67fc22a3fdc4
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-06-01 14:26:52 +0800

    update oop_test.py file as a reading text

--- oop_test.py
+++ oop_test.py
@@ -46,3 +46,11 @@ def convert(snippet, phrase):
              random.sample(WORDS, param_count)))
     for sentence in snippet, phrase:
         result = sentence[:]
+
+        # fake class other_names
+        for word in class_names:
+            result = result.replace("%%%", word, 1)
+
+        # fake other class_names
+        for word in other_names:
+            result = result.replace("***", word, 1)
