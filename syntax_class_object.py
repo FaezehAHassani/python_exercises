29102ee42b2cf3fcb175e75993230f0b6b5c0a91
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-06-02 15:48:51 +0800

    update syntax file for class and object

--- syntax_class_object.py
+++ syntax_class_object.py
@@ -1,4 +1,4 @@
-# class A(B) => class A is-a B (object)
+# class A(B) => class A is-a B (object), A is-a B
 # class A(B):   => class A is-a B and has-a __init__ that takes self and C params
 #    def __init__(self, C) => C is called attribute
 
@@ -7,6 +7,10 @@ class Animal(object):
 
 # class Dog is-a Animal that takes __init__ with self and name params, set name attribute to name
 class Dog(Animal):
+    def __init__(self, name):
+        self.name = name
+
+class Cat(Animal):
     def __init__(self, name):
         self.name = name
 
