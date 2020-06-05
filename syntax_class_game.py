9166988705f1f304498bd62443b2ea2464a2721a
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-06-05 15:53:13 +0800

    update syntax file for classes to make a game

--- syntax_class_game.py
+++ syntax_class_game.py
@@ -41,6 +41,16 @@ class TheBridge(Scene):
    def enter(self):
        pass
 
-class EscapePod(Scene): 
+class EscapePod(Scene):
    def enter(self):
        pass
+
+class Map(object):
+    def __init__(self, start_scene):
+        pass
+
+    def next_scene(self, scene_name):
+        pass
+
+    def opening_scene(self):
+        pass
