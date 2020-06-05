a90c29d303eb9a0958f12d6f8843665ec9c1968b
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-06-05 15:44:30 +0800

    create syntax file for classes to make a game

--- syntax_class_game.py
+++ syntax_class_game.py
@@ -1,5 +1,28 @@
 # how to write a test
 
-class Scene(object):
+# first make a tree like below
+#* Map
+#    - next_scene
+#    - opening_scene
+#* Engine
+#    - play
+#* Scene
+#   - enter
+#   * Death
+#   * Central Corridor
+#   * Laser Weapon Armory
+#   * The Bridge
+#   * Escape Pod
+
+class Scene(object): # Scene is-a object
     def enter(self):
         pass
+
+class Engine(object):
+    def __init__(self, scene_map):
+        pass
+
+    def play(self):
+        pass
+
+class Death(Scene): # Death is-a Scene
