1f3ac0d01855051909e02f9c6d83bfc8bfb580db
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-06-07 11:51:38 +0800

    update syntax file for using classes to make a game

--- syntax_class_game.py
+++ syntax_class_game.py
@@ -41,17 +41,18 @@ class Engine(object):
             # print the last scene
             current_scene.enter()
 
-class Death(Scene): # Death is-a Scene
-   quips =[
+class Death(Scene):   # Death is-a Scene
+    quips =[
        "You died. you kinda suck at this.",
        "Your Mom would be proud...if she were smarter.",
        "Such a luser.",
        "I have a small puppy that's better at this.",
        "You're worese than your Dad's jokes."
     ]
-       def enter(self):
-           print(Death.quips[randint(0, len(self.quips)-1)])
-           exit(1)
+
+    def enter(self):
+        print(Death.quips[randint(0, len(self.quips)-1)])
+        exit(1)
 
 
 class CentralCorridor(Scene):
