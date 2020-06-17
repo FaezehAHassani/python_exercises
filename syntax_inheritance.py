f832b1e4ed5eaccec4bf33dcbe0af63bf8532af3
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-06-17 16:13:43 +0800

    update syntax file for inheritance

--- syntax_inheritance.py
+++ syntax_inheritance.py
@@ -5,46 +5,32 @@ class Parent(object):
     def implicit(self):
         print("PARENT implicit()")
 
-class Child(Parent):
-    pass
-
-dad = Parent()
-son = Child()
-
-dad.implicit()
-son.implicit()
-    
-class Parent(object):
-
     def override(self):
         print("PARENT override()")
-
-class Child(Parent):
-
-    def override(self):
-        print("CHILD override()")
-
-dad = Parent()
-son = Child()
-
-dad.override()
-son.override()
-
-
-class Parent(object):
 
     def altered(self):
         print("PARENT altered()")
 
 class Child(Parent):
 
+    def override(self):
+        print("CHILD override()")
+
     def altered(self):
         print("CHILD, BEFORE PARENT altered()")
         super(Child, self).altered()   # here calls back to altered in PARENT
         print("CHILD, AFTER PARENT altered()")
 
+
 dad = Parent()
 son = Child()
+
+dad.implicit()
+son.implicit()
+
+
+dad.override()
+son.override()
 
 dad.altered()
 son.altered()
