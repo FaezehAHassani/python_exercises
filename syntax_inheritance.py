36ba5c1cd3ad40f79ae19a81b5bf74f6ebe89b6b
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-06-17 16:00:43 +0800

    update syntax file for inheritance

--- syntax_inheritance.py
+++ syntax_inheritance.py
@@ -1,1 +1,17 @@
 # inheritance
+
+class Parent(object):
+
+    def override(self):
+        print("PARENT override()")
+
+class child(Parent):
+
+    def override(self):
+        print("CHILD override()")
+
+dad = Parent()
+son = Child()
+
+dad.override()
+son.override()
