c0a5d02fa767d1008ea0976fa90d0cb69474da27
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-06-22 14:55:33 +0800

    update my game using class

--- syntax_class_mygame.py
+++ syntax_class_mygame.py
@@ -13,13 +13,27 @@
 
 from sys import exit
 
-class Scene(object): 
+class Scene(object):
     def enter(self):
         print("This scene is not yet configured.")
         print("Subclass it and implement enter()")
         exit(1)
 
-class Hall(object):
+class Engine(object):
+    def __init__(self, scene_map):
+        self.scene_map = scene_map
+
+    def play(self):
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished')
+
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
+
+        current_scene.enter()
+
+class Hall(Scene):
     def enter(self):
         print("Welcome to my Gold Coins game.")
         print("A fantastic entrance hall, isn't it?")
