8cfbc216158d8cb8c43e48fa2cb6fd4a8c77fa66
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-06-24 18:16:31 +0800

    update setup.py for my project skeleton

--- projects/skeleton/tests/setup.py
+++ projects/skeleton/tests/setup.py
@@ -2,3 +2,13 @@ try:
     from setuptools import setuptools
 except ImportError:
     from distutils.core import setup
+
+config = {
+    'description' : 'My Project',
+    'author' : 'Faezeh',
+    'url' : 'https://www.linkedin.com/in/faezeh-arab-hassani-11318317b/',
+    'download_url' : 'Where to download it',
+    'author_email' : 'faezeh@go.com',
+    'version' : '0.1',
+    'instal_requires' : ['nose'],
+}
